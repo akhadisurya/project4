3e3134c5ab6cb54218e6d591199a9d625d292876
Arvin Hadisurya <akhadisurya@gmail.com>
2016-05-19 18:41:06 -0700

    database works

--- config/seeds.js
+++ config/seeds.js
@@ -1,14 +1,24 @@
+// load the env vars
+require('dotenv').load();
+
 var mongoose = require('./database');
 
 var Feed = require('../models/Feed');
 
-var feeds = [
-
+var feeds = [{
+  title: "Harry Potter",
+  body: "Harry Potter is a pothead",
+  score: 10,
+  author: "Arvin",
+  thumbnail: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSFqma8KKJsvHdpNJE9rZh4p6SxjVGefgxkq5e06JLphk0Xcp_KXA",
+  created: new Date(),
+  updated: new Date()
+  }
 ];
 
 Feed.remove({}, function(err) {
   if (err) console.log(err);
-  Feed.create(users, function(err, feeds) {
+  Feed.create(feeds, function(err, feeds) {
     if (err) {
       console.log(err);
     } else {
--- models/feed.js
+++ models/feed.js
@@ -1,19 +1,18 @@
 var mongoose = require('mongoose');
-var ObjectId = mongoose.Schema.Types.ObjectId;
+
 
 var feedSchema = new mongoose.Schema({
-  ofObjectId: [Schema.Types.ObjectId],
   title: {type: String, required: "true"},
   body: {type: String, required: "true"},
   score: {type: Number, min: 1, max: 100, default: 0},
   author: {type: String, required: "true"},
   thumbnail: {type: String, required: "true"},
-  created: {type: Date, default: Date.now}
+  created: {type: Date, default: Date.now},
   updated: {type: Date, default: Date.now}
 });
 
 
-var User = mongoose.model('User', userSchema);
+var Feed = mongoose.model('Feed', feedSchema);
 
-module.exports = User;
+module.exports = Feed;
 

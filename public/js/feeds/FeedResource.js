615e114547dbc5c76cb020b69cfd391f07de5b24
Arvin Hadisurya <akhadisurya@gmail.com>
2016-05-21 09:53:52 -0700

    feed resource

---
+++ public/js/feeds/FeedResource.js
@@ -0,0 +1,15 @@
+(function() {
+  angular.module('ProjectFour')
+    .factory("FeedResource", FeedResource);
+
+  FeedResource.$inject = ['$resource'];
+
+  function FeedResource($resource) {
+    return $resource(
+      "/api/feeds/:id",
+      {id: '@id'}, {
+        'update': { method: 'PUT'}
+      }
+    );
+  }
+})();

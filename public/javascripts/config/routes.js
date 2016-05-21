908acfcd3f1bdd3236a71f2b8140604c34fd6cfa
Arvin Hadisurya <akhadisurya@gmail.com>
2016-05-21 09:42:45 -0700

    config routes.js

---
+++ public/javascripts/config/routes.js
@@ -0,0 +1,40 @@
+(function() {
+  angular.module('ProjectFour')
+    .config(MainRouter);
+
+  MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function MainRouter($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html'
+      })
+      .state('FeedList', {
+        url: '/feeds/list',
+        templateUrl: "js/feeds/feed-list.html",
+        controller: 'FeedListController',
+        controllerAs: 'feedListVm'
+      })
+      .state('ShowFeed', {
+        url: '/feeds/feed/:id',
+        templateUrl: 'js/feeds/show-feed.html',
+        controller: 'ShowFeedController',
+        controllerAs: 'showFeedVm'
+      })
+      .state('FeedNew', {
+        url: '/feeds/new',
+        templateUrl: 'js/feeds/feed-new.html',
+        controller: 'FeedNewController',
+        controllerAs: 'feedNewVm'
+      })
+      .state('feedEdit', {
+        url: '/feeds/edit/:id',
+        templateUrl: 'js/feeds/feed-edit.html',
+        controller: 'FeedEditController',
+        controllerAs: 'feedEditVm'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  }
+})();
